068c7756ee0101ad67588e5dad3253a61ca7d60d
Ali Mousavi <96045566+Ali-Primer@users.noreply.github.com>
2025-02-10 23:18:27 +0330

    Title Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from "react"
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   return (
     <>
-      <div className={`font-roboto w-screen h-screen flex flex-col items-center ${isDarkMode ? 'bg-[var(--dark-body-bg)]' : 'bg-[var(--body-bg)]'}`}>
-        <Header/>
+      <div
+        className={`font-roboto w-screen h-screen flex flex-col items-center ${
+          isDarkMode ? "bg-[var(--dark-body-bg)]" : "bg-[var(--body-bg)]"
+        }`}
+      >
+        <div className="w-11/12 mt-6 lg:w-2/3 lg:mt-12">
+          <Header
+            darkMode={isDarkMode}
+            toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
+          />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,24 @@ import moonIcon from "../assets/icons/moon-icon.svg";
 
 interface Header {
   darkMode: boolean;
-  toggleDarkMode : () => void;
+  toggleDarkMode: () => void;
 }
 
 const Header = ({ darkMode, toggleDarkMode }: Header) => {
   return (
     <>
-      <div className="w-11/12 h-9 mt-6 flex justify-between items-center">
-        <p className="font-bold text-xl">Github Profile Finder</p>
-        <button onClick={toggleDarkMode} className="cursor-pointer flex gap-2 items-center text-sm text-[#697C9A]">
-          {darkMode ? 'LIGHT' : 'DARK'}
-          <img src={darkMode ? sunIcon : moonIcon} alt="sun icon" className="h-4 " />
+      <div className="h-9 flex justify-between items-center">
+        <p className={`font-bold text-xl ${darkMode ? 'text-[var(--dark-text)]' : 'text-[var(--text)]'}`}>Github Profile Finder</p>
+        <button
+          onClick={toggleDarkMode}
+          className={`cursor-pointer flex gap-2 items-center text-sm ${darkMode ? 'text-white' : 'text-[#697C9A]'}`}
+        >
+          {darkMode ? "LIGHT" : "DARK"}
+          <img
+            src={darkMode ? sunIcon : moonIcon}
+            alt="sun icon"
+            className="h-4 "
+          />
         </button>
       </div>
     </>
